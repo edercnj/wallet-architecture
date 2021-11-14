57bf4ea543affa9930f85c68fa88ab4ced9c10f1
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2021-11-14 17:53:40 -0300

    ajuste do docker-compose e tambem script de start

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd wallet-user && ./mvnw clean compile package && cd .. &&
-cd wallet-user && ./mvnw clean compile package && cd ..
+docker-compose down && 
+docker-compose up -d && --remove-orphans
