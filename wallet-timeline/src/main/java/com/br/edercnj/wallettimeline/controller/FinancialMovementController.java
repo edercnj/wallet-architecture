8374b60ea972953831d7d8bbc5d3ca4032fa798b
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2021-11-15 12:30:35 -0300

    ajuste de url de consultar-timeline

--- wallet-timeline/src/main/java/com/br/edercnj/wallettimeline/controller/FinancialMovementController.java
+++ wallet-timeline/src/main/java/com/br/edercnj/wallettimeline/controller/FinancialMovementController.java
@@ -12,9 +12,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +38,22 @@ public class FinancialMovementController {
     @GetMapping(value = "/timeline/financial-movements", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<FinancialMovementDto>> getAllFinancialMovements() {
         List<FinancialMovement> financialMovement = financialMovementService.findAll();
+        List<FinancialMovementDto> financialMovements = mapper.map(financialMovement, new TypeToken<List<FinancialMovement>>() {}.getType());
+        return ResponseEntity.status(HttpStatus.OK).body(financialMovements);
+    }
+
+
+    @ApiOperation(value = "Returns a user's financial timeline")
+    @ApiResponses(value =
+            {
+                    @ApiResponse(code = 200, message = "Query success", response = FinancialMovementDto.class, responseContainer = "List"),
+                    @ApiResponse(code = 404, message = "User not found", response = ErrorResponseDto.class),
+                    @ApiResponse(code = 400, message = "Invalid request parameters", response = ErrorResponseDto.class),
+                    @ApiResponse(code = 500, message = "Internal server error", response = ErrorResponseDto.class)
+            })
+    @GetMapping(value = "/timeline/financial-movements/user/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FinancialMovementDto>> getFinancialMovementsFromUser(@PathVariable(value="id") String userId) {
+        List<FinancialMovement> financialMovement = financialMovementService.findByUserId(userId);
         List<FinancialMovementDto> financialMovements = mapper.map(financialMovement, new TypeToken<List<FinancialMovement>>() {}.getType());
         return ResponseEntity.status(HttpStatus.OK).body(financialMovements);
     }
