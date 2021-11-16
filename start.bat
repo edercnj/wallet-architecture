15cb90fc57f06b86e801297d301e92e3d65e894d
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2021-11-16 01:49:32 -0300

    removido o arquivo .bat

--- start.bat
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd wallet-user && ./mvnw clean compile package && cd .. &&
-cd wallet-timeline && ./mvnw clean compile package && cd .. &&
-cd bill-payment && ./mvnw clean compile package && cd .. &&
-docker-compose down && 
-docker-compose up -d --remove-orphans
