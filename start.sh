3d83aca544ecbf2aae096c4acbead34aa62aca21
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2022-06-22 19:33:38 -0300

    inclusao de dockerfile

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd wallet-user && ./mvnw --quiet clean compile package && cd .. &&
-cd wallet-timeline && ./mvnw --quiet clean compile package && cd .. &&
-cd bill-payment && ./mvnw --quiet clean compile package && cd .. &&
-docker-compose down && 
+cd wallet-user      && ./mvnw --quiet clean compile package     && cd .. &&
+cd wallet-timeline  && ./mvnw --quiet clean compile package     && cd .. &&
+cd bill-payment     && ./mvnw --quiet clean compile package     && cd .. &&
 docker-compose up -d --remove-orphans
