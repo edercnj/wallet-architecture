40f223d299687cbd1da90382c5b8bfe8039c0b9b
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2022-06-23 12:12:49 -0300

    adicionado istio isntall com todas suas dependencias

--- istio-install.sh
+++ istio-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-curl -L https://istio.io/downloadIstio  > ./istio | ISTIO_VERSION=1.14.1 TARGET_ARCH=x86_64 sh - &&
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.14.1 TARGET_ARCH=x86_64 sh - &&
 cd istio-1.14.1/bin && 
-./istioctl install --set profile=demo -y
+./istioctl install --set profile=demo -y  && 
+kubectl label namespace default --overwrite=true stio-injection=enabled &&
+cd .. && kubectl apply -f samples/addons &&
+kubectl rollout status deployment/kiali -n istio-system && 
+cd bin && ./istioctl dashboard kiali
