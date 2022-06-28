073475bec131d34063b4ecffd8916b57f822cc09
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2022-06-28 14:12:10 -0300

    inclusao de arquivos na pasta infra

---
+++ infra/kong/kong-istio-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl create namespace kong-istio && 
+kubectl label namespace kong-istio istio-injection=enabled &&
+helm repo add kong https://charts.konghq.com && helm repo update &&
+helm install -n kong-istio kong-istio kong/kong
--- k3d-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash && 
-k3d cluster create wallet-architecture
